d91f7b712b01f961d8c38d83367b88570aedad95
iaronaraujo <iaron.araujo@ccc.ufcg.edu.br>
2017-07-13 13:41:26 -0300

    Tentei umas coisas pra ajeitar a redundancia

--- src/Analisadores/Simbolos.java
+++ src/Analisadores/Simbolos.java
@@ -8,108 +8,114 @@ package Analisadores;
 /** CUP generated class containing symbol constants. */
 public class Simbolos {
   /* terminals */
-  public static final int comments2 = 93;
-  public static final int comments1 = 92;
-  public static final int switch = 43;
-  public static final int return = 39;
-  public static final int dlthen = 57;
-  public static final int break = 4;
-  public static final int public = 38;
-  public static final int mod = 64;
-  public static final int else = 16;
-  public static final int importstar = 94;
-  public static final int minusequ = 71;
-  public static final int null = 34;
-  public static final int interface = 29;
-  public static final int this = 45;
-  public static final int packag = 35;
+  public static final int comments2 = 97;
+  public static final int comments1 = 96;
+  public static final int switch = 44;
+  public static final int return = 40;
+  public static final int dlthen = 58;
+  public static final int break = 5;
+  public static final int public = 39;
+  public static final int mod = 65;
+  public static final int else = 17;
+  public static final int importstar = 98;
+  public static final int minusequ = 74;
+  public static final int null = 35;
+  public static final int interface = 30;
+  public static final int this = 46;
+  public static final int packag = 36;
   public static final int abstract = 2;
-  public static final int boolean = 3;
-  public static final int for = 23;
-  public static final int do = 13;
-  public static final int try = 50;
-  public static final int dot = 89;
-  public static final int int = 28;
-  public static final int colon = 90;
-  public static final int openkeys = 85;
-  public static final int dec_digits = 96;
-  public static final int closekeys = 86;
-  public static final int quest = 82;
-  public static final int instanceof = 27;
-  public static final int strg = 91;
-  public static final int bthen = 60;
-  public static final int int_literal = 97;
-  public static final int case = 6;
-  public static final int closepar = 66;
-  public static final int multequ = 72;
-  public static final int x = 52;
-  public static final int or = 55;
-  public static final int equequ = 59;
-  public static final int byte = 5;
-  public static final int static = 41;
-  public static final int if = 24;
-  public static final int id = 95;
-  public static final int threadsafe = 46;
-  public static final int l = 30;
-  public static final int tbthen = 63;
-  public static final int bequ = 61;
-  public static final int default = 12;
-  public static final int finally = 21;
-  public static final int lthen = 56;
-  public static final int minusminus = 80;
-  public static final int short = 40;
-  public static final int f = 18;
-  public static final int e = 15;
-  public static final int plusplus = 79;
-  public static final int d = 11;
-  public static final int continue = 10;
-  public static final int new = 33;
-  public static final int modequ = 74;
-  public static final int and = 53;
-  public static final int openbrac = 83;
-  public static final int double = 14;
-  public static final int implements = 25;
-  public static final int synchronized = 44;
-  public static final int ne = 78;
-  public static final int xor = 54;
-  public static final int extend = 17;
-  public static final int comma = 87;
-  public static final int catch = 7;
-  public static final int supe = 42;
-  public static final int openpar = 65;
-  public static final int throw = 47;
+  public static final int minus = 67;
+  public static final int boolean = 4;
+  public static final int for = 24;
+  public static final int do = 14;
+  public static final int div = 76;
+  public static final int ampersand = 3;
+  public static final int try = 51;
+  public static final int dot = 93;
+  public static final int int = 29;
+  public static final int colon = 94;
+  public static final int openkeys = 89;
+  public static final int dec_digits = 100;
+  public static final int closekeys = 90;
+  public static final int quest = 86;
+  public static final int instanceof = 28;
+  public static final int strg = 95;
+  public static final int bthen = 61;
+  public static final int int_literal = 101;
+  public static final int case = 7;
+  public static final int closepar = 69;
+  public static final int multequ = 75;
+  public static final int x = 53;
+  public static final int or = 56;
+  public static final int equequ = 60;
+  public static final int byte = 6;
+  public static final int static = 42;
+  public static final int if = 25;
+  public static final int id = 99;
+  public static final int threadsafe = 47;
+  public static final int l = 31;
+  public static final int tbthen = 64;
+  public static final int bequ = 62;
+  public static final int default = 13;
+  public static final int finally = 22;
+  public static final int lthen = 57;
+  public static final int minusminus = 84;
+  public static final int short = 41;
+  public static final int f = 19;
+  public static final int e = 16;
+  public static final int plusplus = 83;
+  public static final int d = 12;
+  public static final int new = 34;
+  public static final int modequ = 78;
+  public static final int and = 54;
+  public static final int openbrac = 87;
+  public static final int double = 15;
+  public static final int implements = 26;
+  public static final int synchronized = 45;
+  public static final int ne = 82;
+  public static final int xor = 55;
+  public static final int extend = 18;
+  public static final int comma = 91;
+  public static final int catch = 8;
+  public static final int supe = 43;
+  public static final int openpar = 68;
+  public static final int throw = 48;
   public static final int EOF = 0;
-  public static final int closebrac = 84;
-  public static final int xorequ = 68;
-  public static final int lequ = 58;
-  public static final int plusequ = 70;
-  public static final int false = 19;
-  public static final int private = 36;
+  public static final int closebrac = 88;
+  public static final int xorequ = 71;
+  public static final int lequ = 59;
+  public static final int plusequ = 73;
+  public static final int false = 20;
+  public static final int private = 37;
   public static final int error = 1;
-  public static final int final = 20;
-  public static final int divequ = 73;
-  public static final int float = 22;
-  public static final int neequ = 77;
-  public static final int clas = 9;
-  public static final int transient = 48;
-  public static final int equ = 67;
-  public static final int andequ = 69;
-  public static final int sorequ = 75;
-  public static final int dbthen = 62;
-  public static final int impor = 26;
-  public static final int orequ = 76;
-  public static final int while = 51;
-  public static final int true = 49;
-  public static final int native = 32;
-  public static final int long = 31;
-  public static final int binneg = 81;
-  public static final int semic = 88;
-  public static final int char = 8;
-  public static final int protected = 37;
+  public static final int final = 21;
+  public static final int divequ = 77;
+  public static final int float = 23;
+  public static final int text = 102;
+  public static final int CONTINUE = 11;
+  public static final int neequ = 81;
+  public static final int clas = 10;
+  public static final int transient = 49;
+  public static final int equ = 70;
+  public static final int andequ = 72;
+  public static final int sorequ = 79;
+  public static final int dbthen = 63;
+  public static final int impor = 27;
+  public static final int orequ = 80;
+  public static final int while = 52;
+  public static final int true = 50;
+  public static final int native = 33;
+  public static final int long = 32;
+  public static final int plus = 66;
+  public static final int binneg = 85;
+  public static final int semic = 92;
+  public static final int char = 9;
+  public static final int protected = 38;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
   "abstract",
+  "ampersand",
   "boolean",
   "break",
   "byte",
@@ -117,7 +123,7 @@ public class Simbolos {
   "catch",
   "char",
   "clas",
-  "continue",
+  "CONTINUE",
   "d",
   "default",
   "do",
@@ -172,6 +178,8 @@ public class Simbolos {
   "dbthen",
   "tbthen",
   "mod",
+  "plus",
+  "minus",
   "openpar",
   "closepar",
   "equ",
@@ -180,6 +188,7 @@ public class Simbolos {
   "plusequ",
   "minusequ",
   "multequ",
+  "div",
   "divequ",
   "modequ",
   "sorequ",
@@ -204,7 +213,8 @@ public class Simbolos {
   "importstar",
   "id",
   "dec_digits",
-  "int_literal"
+  "int_literal",
+  "text"
   };
 }
 
--- src/Geradores/AnalisadorSintatico.bat
+++ src/Geradores/AnalisadorSintatico.bat
@@ -2,5 +2,5 @@ SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_121\bin"
 SET PATH = %JAVA_HOME%;%PATH%
 SET CLASSPATH = %JAVA_HOME%;
 cd C:\Users\iaron\git\Compiladores\src\Analisadores
-java -jar C:\Users\iaron\Desktop\CompilerSource\java-cup-11b.jar -parser Analisador_Sintatico -symbols Simbolos AnalisadorSintatico.cup
+java -jar C:\Users\iaron\Desktop\CompilerSource\java-cup-11b.jar -expect 1200 -parser Analisador_Sintatico -symbols Simbolos AnalisadorSintatico.cup
 pause
